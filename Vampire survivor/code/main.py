cadb582baff4407554cdef048ec096b83b1c6588
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-06-20 22:12:32 +0900

    making a level and collision

--- Vampire survivor/code/main.py
+++ Vampire survivor/code/main.py
@@ -1,7 +1,9 @@
 from settings import *
 from player import Player
 from random import randint
-from sprites import CollisonSprite
+from sprites import *
+from pytmx.util_pygame import load_pygame
+from os.path import join
 
 class Game:
     def __init__(self):
@@ -14,13 +16,23 @@ class Game:
         self.all_sprites = pygame.sprite.Group()
         self.collison_sprites = pygame.sprite.Group()
         
+        self.setup()
+        
         #sprites
         self.player = Player((400,300),self.all_sprites,self.collison_sprites)
-        for i in range(6):
-            x, y = randint(0,WINDOW_WIDTH) , randint(0, WINDOW_HEIGHT)
-            w, h = randint(50,100), randint(100,200)
-            CollisonSprite((x,y), (w,h), (self.all_sprites,self.collison_sprites))
-            
+        
+    
+    def setup(self):
+        map = load_pygame(join('data','maps','world.tmx'))
+        for x,y, image in map.get_layer_by_name('Ground').tiles():
+            Sprite((x * TILE_SIZE ,y * TILE_SIZE), image, self.all_sprites)
+
+        for obj in map.get_layer_by_name('Objects'):
+            CollisonSprite((obj.x, obj.y), obj.image,(self.all_sprites,self.collison_sprites))
+        
+        for collision in map.get_layer_by_name('Collisions'):
+            CollisonSprite((collision.x, collision.y), pygame.Surface((collision.width, collision.height)),self.collison_sprites)
+       
     def run(self):
         while self.running:
             
