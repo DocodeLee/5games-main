0539ad7775af342dba77e7539f14bd5e44cef5ed
DocodeLee <167724096+DocodeLee@users.noreply.github.com>
2024-06-21 10:19:08 +0900

    enemy's collision

--- Vampire survivor/code/main.py
+++ Vampire survivor/code/main.py
@@ -1,6 +1,6 @@
 from settings import *
 from player import Player
-from random import randint
+from random import randint, choice
 from sprites import *
 from pytmx.util_pygame import load_pygame
 
@@ -12,21 +12,43 @@ class Game:
         pygame.display.set_caption("Vampire Sur")
         self.clock = pygame.time.Clock()
         self.running = True
+        
         # groups
         self.all_sprites = AllSprites()
         self.collison_sprites = pygame.sprite.Group()
         self.bullet_sprites = pygame.sprite.Group()
-        self.load_images()
-        self.setup()
+        self.enemies_sprites = pygame.sprite.Group()
+        
+        
         
         #gun timer
         self.can_shoot = True
         self.shoot_time = 0
         self.gun_cooldown = 200
         
+        #enemy timer
+        self.enemy_event = pygame.event.custom_type()
+        pygame.time.set_timer(self.enemy_event,300)
+        self.spawn_position = []
+        
+    
+        self.load_images()
+        self.setup()
+        
+        
     def load_images(self):
         self.bullet_surf = pygame.image.load(join('images','gun', 'bullet.png')).convert_alpha()
         
+        folders = list(walk(join('images', 'enemies')))[0][1]
+        self.enemy_frames = {}
+        for folder in folders:
+            for folder_path, _, file_names in walk(join('images','enemies', folder)):
+                self.enemy_frames[folder] = []
+                for file_name in sorted(file_names, key = lambda name: int(name.split('.')[0])): 
+                    full_path = join(folder_path,file_name)
+                    surf = pygame.image.load(full_path).convert_alpha()
+                    self.enemy_frames[folder].append(surf)
+
     def input(self):
         if pygame.mouse.get_pressed()[0] and self.can_shoot:
             pos = self.gun.rect.center + self.gun.player_direction * 20
@@ -57,7 +79,8 @@ class Game:
             if marker.name == 'Player':
                 self.player = Player((marker.x, marker.y), self.all_sprites, self.collison_sprites)
                 self.gun =Gun(self.player,self.all_sprites)
-            
+            else:
+                self.spawn_position.append((marker.x , marker.y))
     
     
         
@@ -65,10 +88,12 @@ class Game:
         while self.running:
             
             dt = self.clock.tick() / 1000
-    
+            #event loop
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                if event.type == self.enemy_event:
+                    Enemies(choice(self.spawn_position), choice(list(self.enemy_frames.values())), (self.all_sprites,self.enemies_sprites),self.player, self.collison_sprites)
             
             #update
             self.gun_timer()
@@ -79,7 +104,7 @@ class Game:
             self.display_surface.fill((0,0,0))
             self.all_sprites.draw(self.player.rect.center)
             pygame.display.update()
-            print(self.bullet_sprites)
+            
             
         pygame.quit()
                 
